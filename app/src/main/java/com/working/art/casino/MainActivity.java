526fe7bbe0b7f673be7d0de9e53e199c0dc21aea
admin <asu.malchenko@gmail.com>
2018-12-12 15:07:24 +0200

    Basic Version (with apk)

--- app/src/main/java/com/working/art/casino/MainActivity.java
+++ app/src/main/java/com/working/art/casino/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.TextView;
@@ -25,6 +27,9 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         playBalance = findViewById(R.id.balanceText);
